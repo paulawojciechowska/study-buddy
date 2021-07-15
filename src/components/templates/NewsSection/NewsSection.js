d5f5754a6039bf7c4f80c691288863aa78854ffa
astropaula <wojciechowska.fran@gmail.com>
2021-07-15 16:54:57 +0200

    added DatoCMS to NewsSection

--- src/components/templates/NewsSection/NewsSection.js
+++ src/components/templates/NewsSection/NewsSection.js
@@ -1,47 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Wrapper, NewsSeactionHeader, ArticleWrapper, TitleWrapper, ContentWrapper } from './NewsSection.styles';
 import { Button } from 'components/atoms/Button/Button';
-
-const data = [
-  {
-    title: 'New tree at shool yard',
-    category: 'school lifestyle',
-    content:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    image: 'https://unsplash.it/350/250',
-  },
-  {
-    title: 'Sta Wars game',
-    category: 'tech news',
-    content:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ',
-    image: null,
-  },
-  {
-    title: 'Bus shedule',
-    category: 'school lifestyle',
-    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    image: 'https://unsplash.it/350/250',
-  },
-];
+import axios from 'axios';
 
 const NewsSection = () => {
+  const [articles, setArticles] = useState([]);
+  const [error, setError] = useState('');
+  useEffect(() => {
+    axios
+      .post(
+        'https://graphql.datocms.com/',
+        {
+          query: `
+      {
+        allArticles {
+          id
+          title
+          category
+          content
+          image {
+            url
+          }
+        }
+      }`,
+        },
+        {
+          headers: {
+            authorization: `Bearer ${process.env.REACT_APP_DATOCMS_TOKEN}`,
+          },
+        }
+      )
+      .then(({ data: { data } }) => {
+        setArticles(data.allArticles);
+      })
+      .catch((err) => {
+        setError("Sorry We coundn't load the articles");
+      });
+  }, []);
   return (
     <Wrapper>
       <NewsSeactionHeader>University news feed</NewsSeactionHeader>
-      {data.map(({ title, category, content, image = null }) => (
-        <ArticleWrapper key={title}>
-          <TitleWrapper>
-            <h3>{title}</h3>
-            <p>{category}</p>
-          </TitleWrapper>
-          <ContentWrapper>
-            <p>{content}</p>
-            {image ? <img src={image} alt="article" /> : null}
-          </ContentWrapper>
-          <Button isBig>click me</Button>
-        </ArticleWrapper>
-      ))}
+      {articles.length > 0 && !error ? (
+        articles.map(({ title, category, content, image = null }) => (
+          <ArticleWrapper key={title}>
+            <TitleWrapper>
+              <h3>{title}</h3>
+              <p>{category}</p>
+            </TitleWrapper>
+            <ContentWrapper>
+              <p>{content}</p>
+              {image ? <img src={image.url} alt="article" /> : null}
+            </ContentWrapper>
+            <Button isBig>click me</Button>
+          </ArticleWrapper>
+        ))
+      ) : (
+        <NewsSeactionHeader>{error ? error : 'Loading...'}</NewsSeactionHeader>
+      )}
     </Wrapper>
   );
 };
